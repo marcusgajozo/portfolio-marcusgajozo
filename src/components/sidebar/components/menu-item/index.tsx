3a1641ca1c4392ce7258d5b16be10f45d3a8e611
Marcus Gajozo <marcusgajozo@gmail.com>
2026-04-10 20:13:32 -0400

    feat: implement MenuItem component for sidebar navigation

---
+++ src/components/sidebar/components/menu-item/index.tsx
@@ -0,0 +1,19 @@
+import { Icon } from "@/components/icon";
+import { PublicRoutes } from "@/enums/routes";
+import Link from "next/link";
+import { ComponentProps } from "react";
+
+export interface MenuItemProps {
+  name: string;
+  iconName: ComponentProps<typeof Icon>["name"];
+  url: PublicRoutes;
+}
+
+export function MenuItem({ name, iconName, url }: MenuItemProps) {
+  return (
+    <Link href={url} className="flex flex-col items-center gap-2">
+      <Icon name={iconName} />
+      {name}
+    </Link>
+  );
+}
--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,33 +1,37 @@
-import { Icon } from "../icon";
+"use client";
+
+import { useTheme } from "next-themes";
+import { PublicRoutes } from "@/enums/routes";
+import { MenuItem, MenuItemProps } from "./components/menu-item";
+import { Button } from "../button";
+
+const listMenu: MenuItemProps[] = [
+  { name: "Home", iconName: "home", url: PublicRoutes.Home },
+  { name: "Projects", iconName: "folder", url: PublicRoutes.Projects },
+  { name: "Experience", iconName: "suitcase", url: PublicRoutes.Experience },
+  { name: "Contact", iconName: "mail-right", url: PublicRoutes.Contact },
+];
 
 export function Sidebar() {
+  const { resolvedTheme, setTheme } = useTheme();
+
+  const handleToggleTheme = () => {
+    setTheme(resolvedTheme === "dark" ? "light" : "dark");
+  };
+
   return (
-    <nav>
-      <ul className="flex flex-col gap-1">
-        <li>
-          <div>
-            <Icon name="home" />
-            Home
-          </div>
-        </li>
-        <li>
-          <div>
-            <Icon name="folder" />
-            Projects
-          </div>
-        </li>
-        <li>
-          <div>
-            <Icon name="suitcase" />
-            Experience
-          </div>
-        </li>
-        <li>
-          <div>
-            <Icon name="mail-right" />
-            Contact
-          </div>
-        </li>
+    <nav className="h-full relative">
+      <Button variant="ghost" iconName="moon" onClick={handleToggleTheme} />
+      <ul className="flex flex-col items-center justify-center gap-10 h-full">
+        {listMenu.map((item, index) => (
+          <li key={`${item.name}-menu-item-${index}`}>
+            <MenuItem
+              name={item.name}
+              iconName={item.iconName}
+              url={item.url}
+            />
+          </li>
+        ))}
       </ul>
     </nav>
   );
