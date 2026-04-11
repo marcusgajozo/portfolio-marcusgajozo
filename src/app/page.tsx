272459b327b195a03ecac2b7cbfaed2d1cdaed1c
Marcus Gajozo <marcusgajozo@gmail.com>
2026-04-10 22:34:49 -0400

    feat: restructure Home component layout with section and div for improved styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <h1>Hello, World! By Marcus Gajozo! Teste</h1>
+  return (
+    <section className="h-full flex flex-col items-center justify-center">
+      <div className="flex flex-col w-[80%]">
+        <h3>Olá, eu sou o</h3>
+        <h1 className="font-jersey-10 text-8xl">Marcus Gajozo</h1>
+        Hello, World! By Marcus Gajozo! Teste
+      </div>
+    </section>
+  );
 }
