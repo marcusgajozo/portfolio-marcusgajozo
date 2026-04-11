073ec4eac298283a71572d9217150fd5fb933b9f
Marcus Gajozo <marcusgajozo@gmail.com>
2026-04-10 20:13:05 -0400

    feat: enhance theme management by integrating ThemeProvider in RootLayout and updating dark mode styles in globals.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Sidebar } from "@/components/sidebar";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,12 +28,15 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col">
-        <main className="min-h-full flex-1 flex">
-          <Sidebar />
-          <section className="flex-1">{children}</section>
-        </main>
+        <ThemeProvider>
+          <main className="min-h-full flex-1 flex">
+            <Sidebar />
+            <section className="flex-1">{children}</section>
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
