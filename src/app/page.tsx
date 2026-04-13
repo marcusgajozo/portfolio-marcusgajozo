7367d948f12e5530a3469054a470c7744fb5c5e0
Marcus Gajozo <marcusgajozo@gmail.com>
2026-04-13 16:07:50 -0400

    feat: add project showcase with EcoBrasil Aventura and PadFlow to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Icon } from "@/components/icon";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
@@ -94,6 +95,44 @@ export default function Home() {
             <Icon name="file-code" />
             Projetos
           </h2>
+          <section>
+            <div className="flex flex-col gap-2 mt-3 border-2 rounded-lg overflow-hidden w-2xl">
+              <div className="relative w-full aspect-video">
+                <Image
+                  src="/ecobrasil-aventura.png"
+                  alt="EcoBrasil Aventura"
+                  fill
+                  className="object-cover"
+                />
+              </div>
+              <div className="p-4 flex flex-col gap-2">
+                <h3 className="text-xl font-bold">EcoBrasil Aventura</h3>
+                <p>
+                  Projeto inovador de Web 3D focado em educação ambiental,
+                  utilizando React Three Fiber para criar experiências imersivas
+                  otimizadas para hardware de baixo desempenho.
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2 mt-3 border-2 rounded-lg overflow-hidden w-2xl">
+              <div className="relative w-full aspect-video">
+                <Image
+                  src="/padflow.png"
+                  alt="PadFlow"
+                  fill
+                  className="object-cover"
+                />
+              </div>
+              <div className="p-4 flex flex-col gap-2">
+                <h3 className="text-xl font-bold">PadFlow</h3>
+                <p>
+                  Um controlador de áudio web para músicos, com pads contínuos e
+                  uma mesa de efeitos personalizável, controlável remotamente
+                  via smartphone.
+                </p>
+              </div>
+            </div>
+          </section>
         </div>
       </section>
       <section></section>
