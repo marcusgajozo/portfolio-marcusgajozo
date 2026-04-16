9604a820e9477c55018bf86b0257099735e56e93
Marcus Gajozo <marcusgajozo@gmail.com>
2026-04-16 08:06:07 -0400

    feat(home-section): update Home component to include HardSkillSection and ArticlesSection with dynamic headers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,20 @@
-import { LogoItem, LogoLoop } from "@/components/logo-loop";
 import { AboutMeSection } from "./_components/about-me-section";
+import { ArticlesSection } from "./_components/articles-section";
 import { ContactSection } from "./_components/contact-section";
+import { HardSkillSection } from "./_components/hard-skill-section";
 import { HomeSection } from "./_components/home-section";
 import { ProjectsSection } from "./_components/projects-section";
-import {
-  SiNextdotjs,
-  SiReact,
-  SiTailwindcss,
-  SiTypescript,
-} from "react-icons/si";
 
-const techLogos: LogoItem[] = [
-  { node: <SiReact />, title: "React", href: "https://react.dev" },
-  { node: <SiNextdotjs />, title: "Next.js", href: "https://nextjs.org" },
-  {
-    node: <SiTypescript />,
-    title: "TypeScript",
-    href: "https://www.typescriptlang.org",
-  },
-  {
-    node: <SiTailwindcss />,
-    title: "Tailwind CSS",
-    href: "https://tailwindcss.com",
-  },
-];
+// TODO: adicionar o rastreador de mouse em pixel art pixel trail
 
 export default function Home() {
   return (
     <>
       <HomeSection />
-      <AboutMeSection />
-      <LogoLoop
-        logos={techLogos}
-        speed={100}
-        direction="right"
-        logoHeight={30}
-        gap={60}
-        hoverSpeed={0}
-        scaleOnHover
-        fadeOut
-        ariaLabel="Technology partners"
-      />
-      <ProjectsSection />
+      <AboutMeSection number={1} />
+      <HardSkillSection number={2} />
+      <ProjectsSection number={3} />
+      <ArticlesSection number={4} />
       <ContactSection />
     </>
   );
