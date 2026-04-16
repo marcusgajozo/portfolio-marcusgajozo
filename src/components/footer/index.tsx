846edb7a4f4fcba0dbc1f653061e9581851ecc6e
Marcus Gajozo <marcusgajozo@gmail.com>
2026-04-16 13:13:36 -0400

    feat(footer): improve layout and styling of footer component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,12 +3,13 @@ import { Button } from "../button";
 export function Footer() {
   return (
     <footer className="p-5 text-sm border-t justify-center items-center flex">
-      <div className="flex justify-between items-center w-[70%]">
-        <span className="text-xs">
+      <div className="flex flex-wrap-reverse justify-center sm:justify-between items-center w-full max-w-4xl gap-4">
+        <span className="text-xs text-center sm:text-left">
           Copyright © 2026 Marcus Gajozo. <br />
           Todos os direitos reservados.
         </span>
-        <div className="flex items-center justify-center gap-1 mt-2">
+
+        <div className="flex items-center justify-center gap-1">
           <Button iconName="github" variant="ghost" />
           <Button iconName="linkedin" variant="ghost" />
         </div>
