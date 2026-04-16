3f14d9974c2eef24714987c1eb5b523bac3592e5
Marcus Gajozo <marcusgajozo@gmail.com>
2026-04-16 08:05:43 -0400

    feat(articles-section): update ArticlesSection to accept number prop and use SectionHeader

--- src/app/_components/articles-section/index.tsx
+++ src/app/_components/articles-section/index.tsx
@@ -1,11 +1,16 @@
-import { Section } from "@/components/section";
+import { Section, SectionHeader } from "@/components/section";
 
 // TODO: cria seção de artigos
+// TODO: cria a rota para ler o artigoId
+// TODO: criar um rota para listar e pesquisar os arquigos
+interface ArticlesSectionProps {
+  number: number;
+}
 
-export function ArticlesSection() {
+export function ArticlesSection({ number }: ArticlesSectionProps) {
   return (
     <Section className="h-[80%] flex flex-col gap-3 mt-10 justify-center items-center">
-      <h2 className="text-3xl font-medium">Artigos</h2>
+      <SectionHeader title="Artigos" number={number} />
       <p className="max-w-2xl text-center">
         Sinta-se à vontade para entrar em contato e conversarmos mais sobre seus
         projetos.
