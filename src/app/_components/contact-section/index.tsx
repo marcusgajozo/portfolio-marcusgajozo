8f5fe73ba2eb9e6ddc0f890d2bdaab3b2eddc90e
Marcus Gajozo <marcusgajozo@gmail.com>
2026-04-17 18:44:33 -0400

    feat(contact-links): create a new component for contact links and refactor sections to use it

--- src/app/_components/contact-section/index.tsx
+++ src/app/_components/contact-section/index.tsx
@@ -1,8 +1,5 @@
-import { Icon } from "@/components/icon";
+import { ContactLinks } from "@/components/contact-links";
 import { Section } from "@/components/section";
-import Link from "next/link";
-
-// TODO: criar um componente chamado links para contato
 
 export function ContactSection() {
   return (
@@ -12,32 +9,7 @@ export function ContactSection() {
         Sinta-se à vontade para entrar em contato e conversarmos mais sobre seus
         projetos.
       </p>
-      <div className="flex gap-4 mt-4 font-medium">
-        <Link
-          href="https://www.linkedin.com/in/marcus-gajozo/"
-          target="_blank"
-          className="flex gap-1 items-center"
-        >
-          <Icon name="linkedin" />
-          Linkedin
-        </Link>
-        <Link
-          href="https://github.com/marcusgajozo/"
-          target="_blank"
-          className="flex gap-1 items-center"
-        >
-          <Icon name="github" strokeWidth={2} />
-          Github
-        </Link>
-        <Link
-          href="mailto:marcusgajozo@example.com"
-          target="_blank"
-          className="flex gap-1 items-center"
-        >
-          <Icon name="mail-right" />
-          E-mail
-        </Link>
-      </div>
+      <ContactLinks />
     </Section>
   );
 }
--- src/app/_components/home-section/index.tsx
+++ src/app/_components/home-section/index.tsx
@@ -1,6 +1,5 @@
-import { Icon } from "@/components/icon";
+import { ContactLinks } from "@/components/contact-links";
 import { Section } from "@/components/section";
-import Link from "next/link";
 
 export function HomeSection() {
   return (
@@ -15,32 +14,7 @@ export function HomeSection() {
           aplicações web e mobile resolvendo problemas complexos com código
           limpo e eficiente.
         </h3>
-        <div className="flex gap-4 mt-4 font-medium">
-          <Link
-            href="https://www.linkedin.com/in/marcus-gajozo/"
-            target="_blank"
-            className="flex gap-1 items-center"
-          >
-            <Icon name="linkedin" />
-            Linkedin
-          </Link>
-          <Link
-            href="https://github.com/marcusgajozo/"
-            target="_blank"
-            className="flex gap-1 items-center"
-          >
-            <Icon name="github" strokeWidth={2} />
-            Github
-          </Link>
-          <Link
-            href="mailto:marcusgajozo@example.com"
-            target="_blank"
-            className="flex gap-1 items-center"
-          >
-            <Icon name="mail-right" />
-            E-mail
-          </Link>
-        </div>
+        <ContactLinks />
       </section>
     </Section>
   );
