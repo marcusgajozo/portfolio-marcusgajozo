c8b690c69273296dc952b6de2746ec84ca630457
Marcus Gajozo <marcusgajozo@gmail.com>
2026-04-17 09:09:57 -0400

    feat(hard-skill-section): refatorar componente de logos de tecnologias

--- src/app/_components/hard-skill-section/index.tsx
+++ src/app/_components/hard-skill-section/index.tsx
@@ -11,6 +11,8 @@ interface HardSkillSectionProps {
   number: number;
 }
 
+// TODO: romver LogoLoop e criar um table dinamica para receber as tecnologias, e criar um componente de logo para receber o link e o nome da tecnologia, e criar uma animação de fade in e fade out para as logos, e criar uma animação de scale on hover para as logos, e criar uma animação de scroll para as logos, e criar uma animação de scroll infinito para as logos, e criar uma animação de scroll infinito com direção para as logos, e criar uma animação de scroll infinito com velocidade para as logos, e criar uma animação de scroll infinito com gap para as logos, e criar uma animação de scroll infinito com hover speed para as logos, e criar uma animação de scroll infinito com scale on hover para as logos, e criar uma animação de scroll infinito com fade out para as logos, e criar uma animação de scroll infinito com aria label para as logos
+
 const techLogos: LogoItem[] = [
   { node: <SiReact />, title: "React", href: "https://react.dev" },
   { node: <SiNextdotjs />, title: "Next.js", href: "https://nextjs.org" },
