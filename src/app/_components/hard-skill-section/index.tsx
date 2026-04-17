71ee00e0b0de91125d16eb4b8909132a0efc5878
Marcus Gajozo <marcusgajozo@gmail.com>
2026-04-17 17:46:21 -0400

    feat(hard-skill-section): update hard skills list to use technology icons

--- src/app/_components/hard-skill-section/index.tsx
+++ src/app/_components/hard-skill-section/index.tsx
@@ -1,75 +1,34 @@
 import { Section, SectionHeader } from "@/components/section";
 import { Technology } from "@/components/technology";
+import { technologyIcons } from "@/components/technology/constants/technology-icons.constant";
 
-// TODO: ajusta a lista de tecnologias
 interface HardSkillSectionProps {
   number: number;
 }
 
-const hardSkills: HardSkillItemProps[] = [
-  {
-    url: "https://nextjs.org/",
-    name: "Next.js",
-    iconName: "nextjs",
-  },
-  {
-    url: "https://react.dev/",
-    name: "React",
-    iconName: "react",
-  },
-  {
-    url: "https://nestjs.com/",
-    name: "NestJS",
-    iconName: "nestjs",
-  },
-  {
-    url: "https://www.docker.com/",
-    name: "Docker",
-    iconName: "docker",
-  },
-  {
-    url: "https://www.postgresql.org/",
-    name: "PostgreSQL",
-    iconName: "postgresql",
-  },
-  {
-    url: "https://graphql.org/",
-    name: "GraphQL",
-    iconName: "graphql",
-  },
-  {
-    url: "https://gitlab.com/",
-    name: "GitLab",
-    iconName: "gitlab",
-  },
-  {
-    url: "https://tanstack.com/query/latest",
-    name: "TanStack Query",
-    iconName: "tanstack-query",
-  },
-  {
-    url: "https://github.com/",
-    name: "GitHub",
-    iconName: "github",
-  },
-  {
-    url: "https://git-scm.com/",
-    name: "Git",
-    iconName: "git",
-  },
+const hardSkills: (keyof typeof technologyIcons)[] = [
+  "apollo",
+  "docker",
+  "express",
+  "git",
+  "graphql",
+  "jest",
+  "mongodb",
+  "nestjs",
+  "nodejs",
+  "postgresql",
+  "react",
+  "redis",
+  "typescript",
 ];
 
 export function HardSkillSection({ number }: HardSkillSectionProps) {
   return (
     <Section>
-      <SectionHeader title="Hard Skill" number={number} />
-      <section className="mt-2 w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center gap-4">
+      <SectionHeader title="Habilidades" number={number} />
+      <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center gap-4">
         {hardSkills.map((skill, index) => (
-          <Technology
-            key={`skill-${index}-${skill.name}`}
-            url={skill.url}
-            technologyName={skill.iconName}
-          />
+          <Technology key={`skill-${index}-${skill}`} technologyName={skill} />
         ))}
       </section>
     </Section>
